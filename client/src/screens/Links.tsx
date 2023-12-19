e36fe42ffa736b2bbd4e34ec1b39d66049b38731
Сергей Дубров <tmpvarcher@outlook.com>
2023-12-19 13:42:30 +0500

    Links page updated

--- client/src/screens/Links.tsx
+++ client/src/screens/Links.tsx
@@ -1,24 +1,44 @@
 import { Nav } from "react-bootstrap";
-import Header from "../components/Header";
+import React from "react";
+import "../css/Links.css"
+
 
 const LinksScreen = () => {
     return (
-        <>
-        <Header/>
-            <h1>Коммуникации</h1>
-            <div>
-                <Nav.Link href='https://udv.group/about/news/' target="_blank" rel="noopener noreferrer">Официальный сайт UDV Group</Nav.Link>
+        <div className="links-body-container">
+            <div className='tasksContainer'>
+                <div className="tasksHeader">
+                    <a className='homeLogo' href={'/home'}/>
+                    <a className='back-button' href={'/home/welcomepage'}/>
+                    <div>
+                        <h1 className='tasksTitle'>Коммуникации</h1>
+                        <h2 className='tasksSubtitle'>// welcome page</h2>
+                    </div>
+                </div>
             </div>
-            <div>
-                <Nav.Link href='https://t.me/udvdev' target="_blank" rel="noopener noreferrer">Телеграм</Nav.Link>
+
+            <div className='info-container'>
+                <div className="info-container-column">
+                    <div className='info-item'>
+                        <ul className="info-item-list">
+                            <h2 className="info-item-list-title">Коммуникации</h2>
+                            <li>
+                                <Nav.Link href='https://udv.group/about/news/' target="_blank" rel="noopener noreferrer">Официальный сайт UDV Group</Nav.Link>
+                            </li>
+                            <li>
+                                <Nav.Link href='https://t.me/udvdev' target="_blank" rel="noopener noreferrer">Телеграм</Nav.Link>
+                            </li>
+                            <li>
+                                <Nav.Link href='https://vk.ru/udv_dev' target="_blank" rel="noopener noreferrer">Группа ВКонтакте</Nav.Link>
+                            </li>
+                            <li>
+                                <Nav.Link href='https://t.me/udv_qna_bot' target="_blank" rel="noopener noreferrer">Чат-бот по вопросам командировок</Nav.Link>
+                            </li>
+                        </ul>
+                    </div>
+                </div>
             </div>
-            <div>
-                <Nav.Link href='https://vk.ru/udv_dev' target="_blank" rel="noopener noreferrer">Группа ВКонтакте</Nav.Link>
-            </div>
-            <div>
-                <Nav.Link href='https://t.me/udv_qna_bot' target="_blank" rel="noopener noreferrer">Чат-бот по вопросам командировок</Nav.Link>
-            </div>
-        </>
+        </div>
     ) 
 }
 
