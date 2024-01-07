c3903666d672e6800b86142d3922ed68ef92ea7b
Сергей Дубров <tmpvarcher@outlook.com>
2024-01-07 16:24:13 +0500

    Refactoring done

--- client/src/screens/Profile.tsx
+++ client/src/screens/Profile.tsx
@@ -1,54 +1,54 @@
-import Header from '../components/Header';
 import { Container } from 'react-bootstrap';
 import { useHistory } from 'react-router-dom';
 import '../css/Profile.css';
 import React from "react";
 
 const ProfileButton: React.FC = () => {
-  const history = useHistory();
-  const handleButtonClick = () => {
-    history.push('/home/myprofile/profile');
-  };
+    const history = useHistory();
 
-  return (
-    <div>
-      <button onClick={handleButtonClick} className='profileButton'>
-      Мои данные
-    </button>
-    </div>
-  );
+    const handleButtonClick = () => {
+        history.push('/home/myprofile/profile');
+    };
+
+    return (
+        <div>
+            <button onClick={handleButtonClick} className='profileButton'>
+                Мои данные
+            </button>
+        </div>
+    );
 };
 
 const AchievementsButton: React.FC = () => {
-  const history = useHistory();
+    const history = useHistory();
 
-  const handleButtonClick = () => {
-    history.push('/home/myprofile/achievements');
-  };
+    const handleButtonClick = () => {
+        history.push('/home/myprofile/achievements');
+    };
 
-  return (
-    <div>
-      <button onClick={handleButtonClick} className='profileButton'>
-        Достижения
-      </button>
-    </div>
-  );
+    return (
+        <div>
+            <button onClick={handleButtonClick} className='profileButton'>
+                Достижения
+            </button>
+        </div>
+    );
 };
 
 const NotesButton: React.FC = () => {
-  const history = useHistory();
+    const history = useHistory();
 
-  const handleButtonClick = () => {
-    history.push('/home/myprofile/notes');
-  };
+    const handleButtonClick = () => {
+        history.push('/home/myprofile/notes');
+    };
 
-  return (
-    <div>
-      <button onClick={handleButtonClick}>
-        Заметки
-      </button>
-    </div>
-  );
+    return (
+        <div>
+            <button onClick={handleButtonClick}>
+                Заметки
+            </button>
+        </div>
+    );
 };
 
 const ProfileScreen = () => {
@@ -72,6 +72,6 @@ const ProfileScreen = () => {
           </div>
         </body>
     ) 
-}
+};
 
 export default ProfileScreen
